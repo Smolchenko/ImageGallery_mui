099dc387e1a7e3c5f9062e3679212d87170a20c8
Irene Smolchenko <smolchenko@gmail.com>
2023-04-23 15:38:30 +0000

    "Initial commit"

--- src/components/CategoryFilters.js
+++ src/components/CategoryFilters.js
@@ -1,3 +1,37 @@
-export default function CategoryFilter() {
-  return <h1>CategoryFilters</h1>;
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+const StyledButton = styled(Button)({
+  color: "#6096B4",
+  height: 40,
+});
+
+export default function CategoryFilter({ categories, onChange }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginBottom: "10px",
+        "& > *": {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        {categories.map((category) => (
+          <StyledButton
+            key={category}
+            onClick={(event) => onChange(event.target.value)}
+            value={category}
+          >
+            {category}
+          </StyledButton>
+        ))}
+      </ButtonGroup>
+    </Box>
+  );
 }
