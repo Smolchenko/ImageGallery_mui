1a84b9b272af306b96fd457c042eba8ce4090e5b
Irene Smolchenko <smolchenko@gmail.com>
2023-04-24 14:38:42 +0000

    remove comments and rewrite logic part 1

---
+++ src/assets/data.js
@@ -0,0 +1,121 @@
+export const categories = ["Sleep", "Funny", "Inspector", "All"];
+
+export const itemsDataSmall = [
+  {
+    title: "Reggie_business.jpg",
+    img: require("./images/Reggie_business.jpg"),
+  },
+  {
+    title: "Reggie_hungry.jpg",
+    img: require("./images/Reggie_hungry.jpg"),
+  },
+];
+
+export const itemsData = [
+  {
+    title: "Reggie_business.jpg",
+    img: require("./images/Reggie_business.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_hungry.jpg",
+    img: require("./images/Reggie_hungry.jpg"),
+    type: [categories[1], categories[2]],
+  },
+  {
+    title: "Reggie_savana.jpg",
+    img: require("./images/Reggie_savana.jpg"),
+    type: categories[0],
+  },
+  {
+    title: "Reggie_study.jpg",
+    img: require("./images/Reggie_study.jpg"),
+    type: categories[0],
+  },
+  {
+    title: "Reggie_underBed.jpg",
+    img: require("./images/Reggie_underBed.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_caveToy.jpg",
+    img: require("./images/Reggie_caveToy.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_chairNap.jpg",
+    img: require("./images/Reggie_chairNap.jpg"),
+    type: categories[0],
+  },
+  {
+    title: "Reggie_cosmetics.jpg",
+    img: require("./images/Reggie_cosmetics.jpg"),
+    type: categories[2],
+  },
+  {
+    title: "Reggie_hunts1.jpg",
+    img: require("./images/Reggie_hunts1.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_nails.jpg",
+    img: require("./images/Reggie_nails.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_pjms.jpg",
+    img: require("./images/Reggie_pjms.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_roses.jpg",
+    img: require("./images/Reggie_roses.jpg"),
+    type: categories[2],
+  },
+  {
+    title: "Reggie_savana2.jpg",
+    img: require("./images/Reggie_savana2.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_savana3.jpg",
+    img: require("./images/Reggie_savana3.jpg"),
+    type: categories[2],
+  },
+  {
+    title: "Reggie_wsks1.jpg",
+    img: require("./images/Reggie_wsks1.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_balcony.jpg",
+    img: require("./images/Reggie_balcony.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_shoes.jpg",
+    img: require("./images/Reggie_shoes.jpg"),
+    type: categories[2],
+  },
+  {
+    title: "Reggie_savana4.jpg",
+    img: require("./images/Reggie_savana4.jpg"),
+    type: [categories[2], categories[1]],
+  },
+  {
+    title: "Reggie_wsks2.jpg",
+    img: require("./images/Reggie_wsks2.jpg"),
+    type: categories[1],
+  },
+  {
+    title: "Reggie_sleepHug.jpg",
+    img: require("./images/Reggie_sleepHug.jpg"),
+    type: [categories[1], categories[0]],
+  },
+  {
+    title: "Reggie_skin.jpg",
+    img: require("./images/Reggie_skin.jpg"),
+    type: categories[1],
+  },
+  // { title: "lmaoAI.jpeg", img: require("./images/lmaoAI.jpeg") },
+];
