8e778a098eb5011bf7a36eb47f10f2991e8299d3
Irene Smolchenko <smolchenko@gmail.com>
2023-04-24 16:53:48 +0000

    work on slider logic

--- src/components/ImageSizeController.js
+++ src/components/ImageSizeController.js
@@ -8,15 +8,15 @@ const StyledSlider = styled(Slider)({
 });
 const MAX_COLUMNS_SMALL_SCREEN = 3;
 const MAX_COLUMNS_BIG_SCREEN = 5;
-const MIN_COLUMNS_ALL_SCREENS = 0;
+const MIN_COLUMNS_ALL_SCREENS = 1;
 const MIN_STEPS_ALL_SCREENS = 1;
 
-// function getMaxColums(isNarrowScreen, dataLength) {
-//   return Math.min(
-//     isNarrowScreen ? MAX_COLUMNS_SMALL_SCREEN : MAX_COLUMNS_BIG_SCREEN,
-//     dataLength
-//   );
-// }
+function getMaxColums(isNarrowScreen, dataLength) {
+  return Math.min(
+    isNarrowScreen ? MAX_COLUMNS_SMALL_SCREEN : MAX_COLUMNS_BIG_SCREEN,
+    dataLength
+  );
+}
 
 export default function ImageSizeController({
   columnNumber,
@@ -24,11 +24,11 @@ export default function ImageSizeController({
   dataLength,
   isNarrowScreen,
 }) {
-  const columnAmountBigScreen = Math.min(MAX_COLUMNS_BIG_SCREEN, dataLength);
-  const columnAmountSmallScreen = Math.min(
-    MAX_COLUMNS_SMALL_SCREEN,
-    dataLength
-  );
+  // const columnAmountBigScreen = Math.min(MAX_COLUMNS_BIG_SCREEN, dataLength);
+  // const columnAmountSmallScreen = Math.min(
+  //   MAX_COLUMNS_SMALL_SCREEN,
+  //   dataLength
+  // );
   console.log("Slider, dataLength", dataLength);
   console.log("Slider, columnNumber", columnNumber);
   console.log("Slider, isNarrowScreen", isNarrowScreen);
@@ -43,11 +43,11 @@ export default function ImageSizeController({
         valueLabelDisplay="auto"
         marks
         step={MIN_STEPS_ALL_SCREENS}
-        min={MIN_COLUMNS_ALL_SCREENS}
-        max={isNarrowScreen ? columnAmountSmallScreen : columnAmountBigScreen}
+        min={Math.min(MIN_COLUMNS_ALL_SCREENS, dataLength - 1)}
+        // max={isNarrowScreen ? columnAmountSmallScreen : columnAmountBigScreen}
         // max={getMaxColums(isNarrowScreen, dataLength)}
+        max={getMaxColums(isNarrowScreen, dataLength)}
         // max={5}
-        // max={window.innerWidth <= 600 ? 3 : 5}
         disabled={dataLength <= 1}
         value={columnNumber}
         onChange={(event, newValue) => onColumnsChange(newValue)}
